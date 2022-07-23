03f45b70a2ed7aa86aabcf97861270771334eea8
DoktorekHah <lukasz.grabski660@gmail.com>
2022-07-23 23:57:01 +0200

    Im created file named configuration.h and replaced names varbiable in main

---
+++ esp8266_bme280/Configuration.h
@@ -0,0 +1,12 @@
+#define STASSID "ssid"
+#define STAPSK  "password"
+
+#define SNMP_COMMUNITY_STRING "public"
+#define SNMP_COMMUNITY_STRING_RW "private"
+
+#define VALUE_ZERO 0
+
+#define BME280_ADDRESS 0x76
+
+#define SHOW_VALUE_FROM_BME280 0
+#define DELAY 500
